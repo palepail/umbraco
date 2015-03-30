aa246326c4e319b16003ab1ec9d3e1d0d990bf1c
palepail <palepail@gmail.com>
2015-03-30 13:36:28 -1000

    initial push

---
+++ App_Code/Controllers/FormSurfaceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+/// <summary>
+/// Summary description for FormController
+/// </summary>
+
+public class FormSurfaceController : SurfaceController
+{
+    [HttpPost]
+    public ActionResult SaveForm(FormModel model)
+    {    
+   
+        if (!ModelState.IsValid)
+        {
+  
+            return CurrentUmbracoPage();
+        }
+        Console.Write(model.FirstName);
+        
+        return RedirectToCurrentUmbracoPage();
+
+    }
+}
---
+++ App_Code/Models/FormModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+public class FormModel
+{
+    public FormModel(){
+        
+    }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
